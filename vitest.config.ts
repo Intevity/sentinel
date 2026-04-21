f3ddac49fef1f3677c701fe93c95ea4fd4cd201d
jwooden <woodenconsulting@gmail.com>
2026-04-21 13:40:57 -0400

    relax branch cov

--- vitest.config.ts
+++ vitest.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
       thresholds: {
         lines: 95,
         functions: 95,
-        branches: 95,
+        branches: 90,
         statements: 95,
       },
     },
